06de5fbcf2338e6b2c49653cf0c2717edd59cbb9
Tobias Lundin <tobias.lundin@gmail.com>
2021-03-07 15:26:21 +0100

    - add tsconfig to get typings for import.meta - use env file

--- apps/common/src/components/Mega.tsx
+++ apps/common/src/components/Mega.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Mega.scss';
 
+const suffix = import.meta.env.VITE_MEGA_SUFFIX || '';
+
 export const Mega = () => {
   return (
     <>
-      <h1 className="mega">MEGA</h1>
+      <h1 className="mega">MEGA {suffix}</h1>
     </>
   )
 }
