55b274695deb4a13fdccb9035074652809a21725
Erik Mikkelson <ErikMikkelson@gmail.com>
2021-07-02 23:26:52 +0100

    Update dependencies

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,25 +1,26 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 import { name, version } from './package.json';
 import importSvgAsComponent from 'vite-plugin-svgr';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), importSvgAsComponent()],
   resolve: {
     alias: {
-      'SASS_VARIABLES': resolve(__dirname, 'src/styles/common-variables.scss'),
-    }
+      SASS_VARIABLES: resolve(__dirname, "src/styles/common-variables.scss"),
+    },
   },
   define: {
-    pkgJson: { name, version }
+    pkgJson: { name, version },
   },
   build: {
     emptyOutDir: true,
-    outDir: '../../docs',
-  }
-})
+    outDir: "./dist",
+  },
+  server: {
+    open: "/index.html",
+  },
+});
